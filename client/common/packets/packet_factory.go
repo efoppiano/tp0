3eef133264f9a595a9adbc413274d96d895a8b57
efoppiano <efoppiano@fi.uba.ar>
2023-04-06 14:27:14 -0300

    Fix correction 1

--- client/common/packets/packet_factory.go
+++ client/common/packets/packet_factory.go
@@ -2,11 +2,13 @@ package packets
 
 import (
 	"bufio"
+	"encoding/binary"
 	"net"
 	"strings"
 )
 
 const MaxPacketSize = 8192
+const PacketLengthFieldSize = 2
 
 func checkPacketType(data []byte, expected string) error {
 	packetType := strings.Split(string(data), ":")[0]
@@ -25,9 +27,29 @@ func removePacketType(data []byte) []byte {
 	return data[len(packetType)+1:]
 }
 
+func ReadValidLength(reader *bufio.Reader) (uint16, error) {
+	lengthBytes, err := ReadExact(reader, 2)
+	if err != nil {
+		return 0, err
+	}
+	length := binary.BigEndian.Uint16(lengthBytes)
+	if length > MaxPacketSize-PacketLengthFieldSize {
+		return 0, PacketTooBigError{
+			MaxSize: MaxPacketSize - PacketLengthFieldSize,
+		}
+	}
+
+	return length, nil
+}
+
 func ReadRawPacket(conn *net.Conn) ([]byte, error) {
 	reader := bufio.NewReader(*conn)
-	message, err := ReadUntilWithMax(reader, '\n', MaxPacketSize)
+	length, err := ReadValidLength(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	message, err := ReadExact(reader, int(length))
 	if err != nil {
 		return message, err
 	}
